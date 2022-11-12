6ae21deb57fcc37a19d4e80280edd2b1ec1fae7d
eliasmaz <105321861+eliasmaz@users.noreply.github.com>
2022-11-12 23:22:01 +0100

    Delete server directory

--- server/routes/productRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getproductPage, createproductPage, getProductById } from "../controller/productController.js";
-import { checkAdmin } from "../middleware/auth.js";
-
-const router = express.Router();
-router.get("/", getproductPage);
-router.post("/", checkAdmin, createproductPage);
-router.get('/:id', getProductById)
-export default router;
